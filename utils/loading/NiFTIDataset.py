99db2154bd5d0541307471226e68e69e572eaa07
Petronian <lais.peter.7@gmail.com>
2021-09-23 08:29:32 -0400

    train_test_split implemented

--- utils/loading/NiFTIDataset.py
+++ utils/loading/NiFTIDataset.py
@@ -1,16 +1,48 @@
+"""
+Classes and helper functions for reading and splitting data in NiFTI files.
+"""
+
 # pylint: disable=no-name-in-module
 # pylint erroneously fails to recognize function from_numpy in torch.
 
+import sys
+from typing import Tuple
+import os
 from torch import from_numpy
 from torch.utils.data import Dataset
-import os
 import numpy as np
 import pandas as pd
 
+def train_test_split(dataset: "NiFTIDataset") -> Tuple["NiFTIDataset", "NiFTIDataset"]:
+    """
+    Split a NiFTIDatset into two groups (training and testing) based on
+    information specified within its metadata.
+
+    Parameters
+    ----------
+    dataset: the NiFTIDataset to split.
+
+    Returns
+    -------
+    A tuple containing two NiFTIDatasets with training and testing data,
+    respectively.
+    """
+
+    root = dataset.root
+    metadata = dataset.metadata
+    train_mask = metadata['is_Train']
+    transform = dataset.transform
+
+    return (
+        NiFTIDataset(metadata.loc[train_mask,:], root, transform),
+        NiFTIDataset(metadata.loc[~train_mask,:], root, transform)
+    )
+
+
 class NiFTIDataset(Dataset):
     """
-    Dataset responsible for reading in a metadata CSV or DataFrame containing
-    information in the following format:
+    Dataset responsible for reading in a metadata CSV or DataFrame specifying
+    file information for a given hierarchy of data.
 
     Parameters
     ----------
@@ -49,11 +81,11 @@ class NiFTIDataset(Dataset):
 
     def __init__(self, metadata, root, transform=None):
         # Check if root exists.
-        if(not os.path.isdir(root)):
-            exit('ImageDataset: Root does not exist.')
+        if not os.path.isdir(root):
+            sys.exit('ImageDataset: Root does not exist.')
 
         # Load metadata (path_to_csv or dataframe).
-        if (type(metadata) == pd.core.frame.DataFrame):
+        if isinstance(metadata, pd.core.frame.DataFrame):
             metadata_df = metadata.copy()
         else:
             metadata_df = pd.read_csv(metadata)
@@ -64,25 +96,25 @@ class NiFTIDataset(Dataset):
         # Metadata attribute
         self.metadata = metadata_df
         # Image directory
-        self.root = root  
+        self.root = root
 
     def __len__(self):
         # Number of rows of metadata dataframe.
         return len(self.metadata)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         # Extract relevant information.
         image_row = self.metadata.iloc[idx]
         print(image_row)
-        label = image_row['Label'] 
+        label = image_row['Label']
         image_path = os.path.join(self.root, image_row['S25_Path'])
-        
+
         # Load image into numpy array and convert to Tensor.
         image = from_numpy(np.load(image_path))
-        
+
         # Custom transforms.
         if self.transform:
             image = self.transform(image)
-        
+
         # Return image and label.
         return {'image': image, 'label': label}
